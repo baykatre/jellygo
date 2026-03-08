16f5de9af93d1715575f1ea0eb0e86c1b0c45fa3
baykatre <info@anilozturk.com.tr>
2026-03-08 04:19:24 +0300

    Initial Commit

---
+++ JellyGo/JellyGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JellyGoApp.swift
+//  JellyGo
+//
+//  Created by Anıl Öztürk on 8.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct JellyGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
